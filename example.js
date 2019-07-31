591591f3e7671c0c857b9cb57930ea8b96348fe1
Hoang Nam <hoangnam.github@gmail.com>
2019-07-31 09:44:21 +0700

    test

---
+++ example.js
@@ -0,0 +1,8 @@
+function doClick() {
+    let change = document.getElementById("demo");
+    if(change.innerHTML = "Hello HTML") {
+        change.innerHTML = "Hello JS";
+    } else {
+        change.innerHTML = "Hello HTML";
+    }
+}
